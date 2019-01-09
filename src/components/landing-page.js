99da46539155d45ae3abaf0ebc9e183d91c6d226
Bryce Hasshaw <bhasshaw@gmail.com>
2019-01-09 14:44:28 -0800

    first commit

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -5,14 +5,13 @@ import {Link, Redirect} from 'react-router-dom';
 import LoginForm from './login-form';
 
 export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
+    
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
 
     return (
         <div className="home">
-            <h2>Welcome to Foo App</h2>
             <LoginForm />
             <Link to="/register">Register</Link>
         </div>
